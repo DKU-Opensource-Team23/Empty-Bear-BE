3d354af2e5ed85ec56450b1ffd7817c10ecbe934
johaeunn <jheun04@naver.com>
2026-04-02 23:53:50 +0900

    init: Spring Boot 프로젝트 초기 설정

---
+++ src/main/java/com/dku/emptybear/EmptybearApplication.java
@@ -0,0 +1,13 @@
+package com.dku.emptybear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmptybearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmptybearApplication.class, args);
+	}
+
+}
